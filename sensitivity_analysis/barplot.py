daf2dc1a02623786ed130b1afce6fd6d38b04f29
himoto <himoto@protein.osaka-u.ac.jp>
2020-01-07 10:47:08 +0900

    Create reaction.py

--- sensitivity_analysis/barplot.py
+++ sensitivity_analysis/barplot.py
@@ -2,43 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from .sensitivity import analyze_sensitivity
-
-
-def set_reaction_module():
-    n_module = 15
-    reaction_module = [None]*n_module
-    # ERK_activation
-    reaction_module[0] = [i for i in range(1, 7)]
-    # ERK_dephosphorylation_by_DUSP
-    reaction_module[1] = [i for i in range(47, 57)]
-    # ERK_transport
-    reaction_module[2] = [i for i in range(7, 10)]
-    # RSK_activation
-    reaction_module[3] = [24, 25]
-    # RSK_transport
-    reaction_module[4] = [26]
-    # Elk1_activation
-    reaction_module[5] = [29, 30]
-    # CREB_activation
-    reaction_module[6] = [27, 28]
-    # dusp_production_etc
-    reaction_module[7] = [i for i in range(10, 14)]
-    # DUSP_transport
-    reaction_module[8] = [18, 19]
-    # DUSP_stabilization
-    reaction_module[9] = [14, 15, 20, 21]
-    # DUSP_degradation
-    reaction_module[10] = [16, 17, 22, 23]
-    # cfos_production_etc
-    reaction_module[11] = [i for i in range(31, 35)]
-    # cFos_transport
-    reaction_module[12] = [40, 41]
-    # cFos_stabilization
-    reaction_module[13] = [35, 36, 37, 42, 43, 44]
-    # cFos_degradation
-    reaction_module[14] = [38, 39, 45, 46]
-
-    return reaction_module
+from .reaction import *
 
 
 def draw_vertical_span(reaction_module, num_reaction, width):
@@ -57,8 +21,8 @@ def visualize_sensitivity():
     num_reaction = 57  # Num. of Rate Equations
     width = 0.3
 
-    (s_cFosmRNA, s_PcFos) = analyze_sensitivity()
-    reaction_module = set_reaction_module()
+    (s_cFosmRNA, s_PcFos) = analyze_sensitivity(num_reaction)
+    reaction_module = get_reaction_module()
 
     sort_idx = [0]*num_reaction
     left_end = 0
--- sensitivity_analysis/sensitivity.py
+++ sensitivity_analysis/sensitivity.py
@@ -9,21 +9,19 @@ from .model.param_const import f_params
 from .model.initial_condition import initial_values
 
 
-def solveode(diffeq,y0,tspan,args):
+def solveode(diffeq, y0, tspan, args):
     sol = ode(diffeq)
-    sol.set_integrator('vode',method='bdf',with_jacobian=True)
-    sol.set_initial_value(y0,tspan[0])
+    sol.set_integrator('vode', method='bdf', with_jacobian=True)
+    sol.set_initial_value(y0, tspan[0])
     sol.set_f_params(args)
-
     T = [tspan[0]]
     Y = [y0]
-
     while sol.successful() and sol.t < tspan[-1]:
         sol.integrate(sol.t+1.)
         T.append(sol.t)
         Y.append(sol.y)
 
-    return np.array(T),np.array(Y)
+    return np.array(T), np.array(Y)
 
 
 # Calculation of the duration as the time it takes to decline below 10% of its maximum
@@ -39,46 +37,44 @@ def get_duration(time_course_vector):
     return duration
 
 
-def analyze_sensitivity():
+def analyze_sensitivity(num_reaction):
 
     x = f_params()
     y0 = initial_values()
 
-    condition = 2 # EGF & HRG
-    tspan = range(5401) # -> 90 min.
-    len_v = 57 # Num. of Rate equations (See differential_equation.py)
+    condition = 2  # EGF & HRG
+    tspan = range(5401)  # -> 90 min.
 
-    rate = 1.01 # 1% change
+    rate = 1.01  # 1% change
 
     # Signaling metric
-    duration_cFosmRNA = np.empty((condition,len_v))
-    integ_PcFos = np.empty((condition,len_v))
+    duration_cFosmRNA = np.empty((condition, num_reaction))
+    integ_PcFos = np.empty((condition, num_reaction))
 
-    for j in range(len_v):
-        de.perturbation = [1]*len_v
+    for j in range(num_reaction):
+        de.perturbation = [1]*num_reaction
         de.perturbation[j] = rate
 
         for i in range(condition):
-            if i==0:
+            if i == 0:
                 y0[V.EGF] = 10.0
                 y0[V.HRG] = 0.0
-            elif i==1:
+            elif i == 1:
                 y0[V.EGF] = 0.0
                 y0[V.HRG] = 10.0
+            (T, Y) = solveode(de.diffeq, y0, tspan, tuple(x))
 
-            (T,Y) = solveode(de.diffeq,y0,tspan,tuple(x))
+            cFosmRNA = Y[:, V.cfosmRNAc]
+            PcFos = Y[:, V.pcFOSn]*(x[C.Vn]/x[C.Vc]) + Y[:, V.pcFOSc]
 
-            cFosmRNA = Y[:,V.cfosmRNAc]
-            PcFos = Y[:,V.pcFOSn]*(x[C.Vn]/x[C.Vc]) + Y[:,V.pcFOSc]
+            duration_cFosmRNA[i, j] = get_duration(cFosmRNA)
+            integ_PcFos[i, j] = simps(PcFos)
 
-            duration_cFosmRNA[i,j] = get_duration(cFosmRNA)
-            integ_PcFos[i,j] = simps(PcFos)
-            
-            sys.stdout.write('\r%d/%d'%(1+j,len_v))
-            
+            sys.stdout.write('\r%d/%d' % (1+j, num_reaction))
 
     # Sensitivity coefficient
-    s_cFosmRNA = np.log(duration_cFosmRNA/duration_cFosmRNA[:,0][:,None])/np.log(rate)
-    s_PcFos = np.log(integ_PcFos/integ_PcFos[:,0][:,None])/np.log(rate)
+    s_cFosmRNA = np.log(duration_cFosmRNA /
+                        duration_cFosmRNA[:, 0][:, None])/np.log(rate)
+    s_PcFos = np.log(integ_PcFos/integ_PcFos[:, 0][:, None])/np.log(rate)
 
     return s_cFosmRNA, s_PcFos
